fafc53eecfcadf538fd7308e902c4b14eca0b5f1
doc <doc@localhost.localdomain>
2021-04-05 23:22:38 +0300

    fixed link in component User

--- client/components/user.js
+++ client/components/user.js
@@ -3,20 +3,20 @@ import { Link, useParams } from 'react-router-dom'
 import Head from './head'
 
 const User = () => {
-    const { user } = useParams()
-    return (
-        <div>
-            <Head title="Hello" />
-            <div className="flex items-center justify-center h-screen">
-                <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-                    <div id="title">Profile</div>
-                    <Link to="/dashboard/">Go To Root</Link>
-                    <Link to="/dashboard/main">Go To Main</Link>
-                    <div id="username">{user}</div>
-                </div>
-            </div>
+  const { user } = useParams()
+  return (
+    <div>
+      <Head title="Hello" />
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">Profile</div>
+          <Link to="/dashboard">Go To Root</Link>
+          <Link to="/dashboard/main">Go To Main</Link>
+          <div id="username">{user}</div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 User.propTypes = {}
