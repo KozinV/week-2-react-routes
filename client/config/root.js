21695a0c45c0d1f6b11a5a092f900e95c8638fa3
doc <doc@localhost.localdomain>
2021-04-05 23:53:10 +0300

    added 1 route in route.js for Dash

--- client/config/root.js
+++ client/config/root.js
@@ -9,6 +9,7 @@ import { bindActionCreators } from 'redux'
 import store, { history } from '../redux'
 
 import Home from '../components/home'
+import Dash from '../components/dash'
 import DummyView from '../components/dummy-view'
 import NotFound from '../components/404'
 
@@ -84,6 +85,7 @@ const RootComponent = (props) => {
         <StartupConnected>
           <Switch>
             <Route exact path="/" component={() => <DummyView />} />
+            <Route exact path="/dashboard/" component={() => <Dash />} />
             <Route exact path="/dashboard/*" component={() => <Home />} />
             <PrivateRouteConnected exact path="/hidden-route" component={() => <DummyView />} />
             <Route component={() => <NotFound />} />
